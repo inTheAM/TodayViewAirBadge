eb07c15f5419ab4fd80eb0a744296943d09f122d
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-07-21 20:52:57 +0200

    Initial Commit

---
+++ TodayViewAirBadge/TodayViewAirBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayViewAirBadgeApp.swift
+//  TodayViewAirBadge
+//
+//  Created by Ahmed Mgua on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TodayViewAirBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
